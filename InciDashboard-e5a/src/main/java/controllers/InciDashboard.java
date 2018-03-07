9bd7d2cb4777db11e96c60a462529f5109747afc
Tania Alvarez Diaz <UO244856@uniovi.es>
2018-03-07 20:06:25 +0100

    Creacion controlador inciDashboard

---
+++ InciDashboard-e5a/src/main/java/controllers/InciDashboard.java
@@ -0,0 +1,44 @@
+package controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import services.IncidencesService;
+
+
+@Controller
+public class InciDashboard {
+
+	@Autowired
+	private IncidencesService incidencesService;
+	
+	@RequestMapping("/inciDashboard")
+	public String showInfo(Model model)
+	{
+		//List<Incidence> incidencias = incidencesService.
+		//model.addAttribute("incidencesInfo", incidencias);
+		return "showInfo";
+	}
+	
+	public String getIncidenceInfo(Model model)
+	{
+		return "";
+	}
+	
+	public String modifyInfo(Model model)
+	{
+		return "";
+	}
+	
+	public String updateIncidences(Model model)
+	{
+		return "";
+	}
+	
+	public String getIncidences(Model model)
+	{
+		return "";
+	}
+}
---
+++ InciDashboard-e5a/src/main/java/services/IncidencesService.java
@@ -0,0 +1,15 @@
+package services;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Servicio que accede a la base de datos para modificar
+ * los datos de las incidencias.
+ * 
+ * @author Tania Álvarez Díaz
+ *
+ */
+@Service
+public class IncidencesService {
+
+}
