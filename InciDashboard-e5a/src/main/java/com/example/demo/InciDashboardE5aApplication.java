89bdb0b27897346037ebc6533cba199d6b6b712e
Tania Alvarez Diaz <UO244856@uniovi.es>
2018-03-07 19:12:11 +0100

    Creacion del proyecto

---
+++ InciDashboard-e5a/src/main/java/com/example/demo/InciDashboardE5aApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InciDashboardE5aApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InciDashboardE5aApplication.class, args);
+	}
+}
