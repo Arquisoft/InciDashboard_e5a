419d4d661a1684521576f4826bbad31769b4b6b2
Tania Alvarez Diaz <tani.alvarez.diaz@gmail.com>
2018-03-07 20:56:14 +0100

    Creacion atributos Incidence y creacion clase Estado en la cual se representa el estado de una Incidencia

---
+++ InciDashboard-e5a/src/main/java/entities/Estado.java
@@ -0,0 +1,37 @@
+package entities;
+
+public class Estado {
+
+	private final static int ABIERTA = 0;
+	private final static int EN_PROCESO = 1;
+	private final static int CERRADA = 2;
+	private final static int ANULADA = 3;
+	
+	public int getAbierta()
+	{
+		return ABIERTA;
+	}
+	
+	public int getEnProceso()
+	{
+		return EN_PROCESO;
+	}
+	
+	public int getCerrada()
+	{
+		return CERRADA;
+	}
+	
+	public int getAnulada()
+	{
+		return ANULADA;
+	}
+	
+	/**
+	 * Constructor
+	 */
+	public Estado()
+	{
+		
+	}
+}
--- InciDashboard-e5a/src/main/java/entities/Incidence.java
+++ InciDashboard-e5a/src/main/java/entities/Incidence.java
@@ -2,4 +2,11 @@ package entities;
 
 public class Incidence {
 
+//	private User usuario;
+//	private Password contraseña;
+//	private String descripcion;
+//	private Location localizacion;
+//	private List<String> etiquetas;
+//	private HashMap<String,Integer> campos;
+//	private Estado estado;
 }
