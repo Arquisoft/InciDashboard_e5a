8a79569b6afae11e64931c79051d8b92b0659e90
Ismael Cadenas <35774278+IsmaCadenas@users.noreply.github.com>
2018-03-15 10:48:32 +0100

    Cambio clase por interfaz a IncidencesRepository

--- InciDashboard-e5a/src/main/java/repositories/IncidencesRepository.java
+++ InciDashboard-e5a/src/main/java/repositories/IncidencesRepository.java
@@ -1,12 +1,12 @@
 package repositories;
 
-import java.util.Optional;
 
-import org.apache.catalina.startup.CredentialHandlerRuleSet;
 import org.springframework.data.repository.CrudRepository;
 
-public class IncidencesRepository // extends CrudRepository<Incidence, Long>{
-{
+import entities.Incidence;
+
+public interface IncidencesRepository extends CrudRepository<Incidence, Long>{
+
     // metodo actualizar incidencia
 
 }
