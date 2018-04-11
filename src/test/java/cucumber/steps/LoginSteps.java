773ba2caa440d2c8741829e492b4c45073d1fd41
Tania Alvarez Diaz <tani.alvarez.diaz@gmail.com>
2018-04-11 18:42:01 +0200

    Ampliada clase LoginSteps

--- src/test/java/cucumber/steps/LoginSteps.java
+++ src/test/java/cucumber/steps/LoginSteps.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import com.uniovi.model.User;
 
-import cucumber.api.java.en.Given;
+import cucumber.api.java.en.*;
 
 public class LoginSteps {
 
@@ -12,5 +12,10 @@ public class LoginSteps {
 	public void una_lista_de_usuarios(List<User> usuarios) throws Throwable {
 		for (User u : usuarios)
 			System.out.println(u.getEmail());
+	}
+	
+	@When("^introduzco el email \"(.+)\" y contraseña \"(.+)\"$")
+	public void introduzco_el_email_y_contrasena(String email, String contrasena) throws Throwable {
+		System.out.println("Logeado usuario con email " + email);
 	}
 }
