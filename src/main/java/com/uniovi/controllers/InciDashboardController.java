69f70655addf334cfda320fd0f35b15a6677b01d
Tania Alvarez Diaz <tani.alvarez.diaz@gmail.com>
2018-05-01 15:32:21 +0200

    Modificado InciDashboardController

--- src/main/java/com/uniovi/controllers/InciDashboardController.java
+++ src/main/java/com/uniovi/controllers/InciDashboardController.java
@@ -11,76 +11,76 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
-import com.uniovi.model.Estado;
 import com.uniovi.model.Incidence;
+import com.uniovi.model.Incidence.Estado;
 import com.uniovi.services.IncidencesService;
 
 @Controller
 public class InciDashboardController {
 
-    @Autowired
-    IncidencesService incidenceService;
+	@Autowired
+	IncidencesService incidenceService;
 
-    public List<SseEmitter> emitters = Collections.synchronizedList(new ArrayList<SseEmitter>());
+	public List<SseEmitter> emitters = Collections.synchronizedList(new ArrayList<SseEmitter>());
 
-    @RequestMapping("/listIncidences")
-    public String showInfo(Model model) {
-	List<Incidence> incidencias = incidenceService.getIncidences();
-	model.addAttribute("incidencesList", incidencias);
-	return "/incidences/listIncidences";
-    }
+	@RequestMapping("/listIncidences")
+	public String showInfo(Model model) {
+		List<Incidence> incidencias = incidenceService.getIncidences();
+		model.addAttribute("incidencesList", incidencias);
+		return "/incidences/listIncidences";
+	}
 
-    @RequestMapping("/getEmitter")
-    public SseEmitter getEmitter() {
-	return nuevoEmitter();
-    }
+	@RequestMapping("/getEmitter")
+	public SseEmitter getEmitter() {
+		return nuevoEmitter();
+	}
 
-    public SseEmitter nuevoEmitter() {
-	SseEmitter emitter = new SseEmitter(0L);
+	public SseEmitter nuevoEmitter() {
+		SseEmitter emitter = new SseEmitter(0L);
 
-	emitter.onTimeout(() -> emitters.remove(emitter));
-	emitter.onCompletion(() -> emitters.remove(emitter));
+		emitter.onTimeout(() -> emitters.remove(emitter));
+		emitter.onCompletion(() -> emitters.remove(emitter));
 
-	emitters.add(emitter);
+		emitters.add(emitter);
 
-	return emitter;
-    }
+		return emitter;
+	}
 
-    @RequestMapping("/inciDashboard/detailsIncidence/{id}")
-    public String getIncidenceInfo(Model model, @PathVariable Long id) {
+	@RequestMapping("/inciDashboard/detailsIncidence/{id}")
+	public String getIncidenceInfo(Model model, @PathVariable Long id) {
 
-	model.addAttribute("incidence", incidenceService.getIncidence(id));
+		model.addAttribute("incidence", incidenceService.getIncidence(id));
 
-	return "/incidences/detailsIncidence";
-    }
+		return "/incidences/detailsIncidence";
+	}
 
-    @RequestMapping("/incidence/modify/{id}")
-    public String modifyInfo(Model model, @PathVariable Long id) {
+	@RequestMapping("/incidence/modify/{id}")
+	public String modifyInfo(Model model, @PathVariable Long id) {
 
-	model.addAttribute("incidence", incidenceService.getIncidence(id));
-	model.addAttribute("listStates", Estado.values());
+		model.addAttribute("incidence", incidenceService.getIncidence(id));
+		model.addAttribute("listStates", Estado.values());
 
-	return "/incidences/modifyIncidence";
-    }
+		return "/incidences/modifyIncidence";
+	}
 
-    @RequestMapping("/inciDashboard/modifyStateIncidence/{id}/{state}")
-    public String modifyState(@PathVariable Long id, @PathVariable String state) {
+	@RequestMapping("/inciDashboard/modifyStateIncidence/{id}/{state}")
+	public String modifyState(@PathVariable Long id, @PathVariable Estado state) {
 
-	incidenceService.modifyState(id, state);
+		incidenceService.modifyState(id, state);
 
-	return "redirect:/incidences/listIncidences";
-    }
+		return "redirect:/incidences/listIncidences";
+	}
 
-    @RequestMapping("/login")
-    public String login() {
-	return "login";
-    }
-    
-    @RequestMapping("/verMapa/{latitud}/{longitud}")
+	@RequestMapping("/login")
+	public String login() {
+		return "login";
+	}
+
+	@RequestMapping("/verMapa/{latitud}/{longitud}")
 	public String getMap(Model model, @PathVariable double latitud, @PathVariable double longitud) {
 		model.addAttribute("latitud", latitud);
 		model.addAttribute("longitud", longitud);
 		return "incidences/map";
-}
+	}
 
 }
